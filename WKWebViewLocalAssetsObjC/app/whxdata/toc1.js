90faee09b6c099485adb93d66f6e967177e0d18b
jomart1985 <jomart1985@mail.com>
2023-02-17 07:53:53 -0800

    -----

---
+++ WKWebViewLocalAssetsObjC/app/whxdata/toc1.js
@@ -0,0 +1,1 @@
+gXMLBuffer ="<?xml version=\"1.0\" encoding=\"utf-8\" ?><data  src=\"toc.js\"  name=\"index.php\"><book name=\"forum33\" src=\"toc2.js\" /><book name=\"forum4\" src=\"toc3.js\" /></data>";
---
+++ WKWebViewLocalAssetsObjC/app/whxdata/toc1.new.js
@@ -0,0 +1,4 @@
+(function() {
+var toc =  [{"type":"book","name":"forum33","key":"toc2"},{"type":"book","name":"forum4","key":"toc3"}];
+window.rh.model.publish(rh.consts('KEY_TEMP_DATA'), toc, { sync:true });
+})();
